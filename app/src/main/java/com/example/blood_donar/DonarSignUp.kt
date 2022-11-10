cfdc865b3ee8feb231626ca2c86cea50ff9ed867
Sandip Tamang <sandip.tamang@student.griffith.ie>
2022-11-10 11:39:57 +0000

    Added the sign up page

---
+++ app/src/main/java/com/example/blood_donar/DonarSignUp.kt
@@ -0,0 +1,11 @@
+package com.example.blood_donar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DonarSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.donar_signup)
+    }
+}
