2c1926daeb0ae20fee5a59be7bf1276503467d64
Sandip Tamang <sandip.tamang@student.griffith.ie>
2022-11-10 10:43:39 +0000

    Added the search donar page

--- app/src/main/java/com/example/blood_donar/MainActivity.kt
+++ app/src/main/java/com/example/blood_donar/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.blood_donar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _searchDonar : Button
+    private lateinit var _becomeDonar : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _searchDonar = findViewById(R.id.searchDonar)
+        _searchDonar.setOnClickListener {
+            Log.i("donar", "you clicked search donar button")
+        }
     }
 }
