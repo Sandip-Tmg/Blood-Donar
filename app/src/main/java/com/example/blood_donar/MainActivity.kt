3ad667e14c8d9939079759164f85b3718d06c9ba
Sandip Tamang <sandip.tamang@student.griffith.ie>
2022-11-12 11:10:39 +0000

    Fixed the crash error when clicking on button to show the activity

--- app/src/main/java/com/example/blood_donar/MainActivity.kt
+++ app/src/main/java/com/example/blood_donar/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.blood_donar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
