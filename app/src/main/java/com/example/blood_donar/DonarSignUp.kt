b37af652f81304308687a7ff9e674768a32678aa
Sandip Tamang <sandip.tamang@student.griffith.ie>
2022-11-23 16:06:52 +0000

    Added the dropdown option for search and sign up donar page

--- app/src/main/java/com/example/blood_donar/DonarSignUp.kt
+++ app/src/main/java/com/example/blood_donar/DonarSignUp.kt
@@ -1,11 +1,45 @@
 package com.example.blood_donar
 
 import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+
 class DonarSignUp : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.donar_signup)
+
+        // code for making a dropdown option for blood type
+        val bloodType = arrayOf("A+","A-","B+","B-","O+","O-","AB+","AB-")
+        val bloodAutoTextview = findViewById<AutoCompleteTextView>(R.id.blood_type_select)
+        val bloodAdapter = ArrayAdapter(this,R.layout.item_type,bloodType)
+        bloodAutoTextview.setAdapter(bloodAdapter)
+
+        // code for making a dropdown option for sex type
+        val sex = arrayOf("Male","Female","Other")
+        val sexAutoTextView = findViewById<AutoCompleteTextView>(R.id.sex_type_select)
+        var sexAdapter = ArrayAdapter(this,R.layout.item_type, sex)
+        sexAutoTextView.setAdapter(sexAdapter)
+
+        // event to show the toast message of selected option when selecting the option
+        bloodAutoTextview.onItemClickListener = OnItemClickListener { parent, view, position, rowId ->
+            val selection = parent.getItemAtPosition(position) as String
+            Toast.makeText(applicationContext, "Type: $selection", Toast.LENGTH_SHORT).show()
+        }
+
+        // event to show the toast message of selected option when selecting the option
+        sexAutoTextView.onItemClickListener = OnItemClickListener { parent, view, position, rowId ->
+            val selection = parent.getItemAtPosition(position) as String
+            Toast.makeText(applicationContext, "Type: $selection", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/blood_donar/SearchDonar.kt
+++ app/src/main/java/com/example/blood_donar/SearchDonar.kt
@@ -1,6 +1,10 @@
 package com.example.blood_donar
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchDonar : AppCompatActivity() {
@@ -8,5 +12,33 @@ class SearchDonar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_donar)
+
+        // code for making a dropdown option for blood type
+        val bloodType = arrayOf("A+","A-","B+","B-","O+","O-","AB+","AB-")
+        val bloodAutoTextview = findViewById<AutoCompleteTextView>(R.id.blood_type_select)
+        val bloodAdapter = ArrayAdapter(this,R.layout.item_type,bloodType)
+        bloodAutoTextview.setAdapter(bloodAdapter)
+
+        // code for making a dropdown option for sex type
+        val range = arrayOf("10km","20km","50km","100km")
+        val rangeAutoTextView = findViewById<AutoCompleteTextView>(R.id.range_type_select)
+        var rangeAdapter = ArrayAdapter(this,R.layout.item_type, range)
+        rangeAutoTextView.setAdapter(rangeAdapter)
+
+        // event to show the toast message of selected option when selecting the option
+        bloodAutoTextview.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, rowId ->
+                val selection = parent.getItemAtPosition(position) as String
+                Toast.makeText(applicationContext, "Type: $selection", Toast.LENGTH_SHORT).show()
+            }
+
+        // event to show the toast message of selected option when selecting the option
+        rangeAutoTextView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, rowId ->
+                val selection = parent.getItemAtPosition(position) as String
+                Toast.makeText(applicationContext, "Type: $selection", Toast.LENGTH_SHORT).show()
+            }
+
+
     }
 }
